5acb4f7de4bb782b2f521c0d3ee3ecdf9cc5fb15
Shubham Raturi <61511486+shubhamraturi37@users.noreply.github.com>
2020-12-08 23:31:46 +0530

    Add files via upload

---
+++ my_template.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: my template
+ * author: shubham
+ */
+get_header();
+?>
+<html>
+    <body>
+        <h3>this is html content</h3>
+    </body>
+</html>
+<?php 
+get_footer();
+?>
